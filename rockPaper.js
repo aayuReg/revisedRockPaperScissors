b7948823cd0e42a7f7d4390958e2c569382e82be
Aayush Regmi <aayushreg10@gmail.com>
2022-11-20 12:07:11 +0545

    Almost completed using DOM; Only Score Is left

--- rockPaper.js
+++ rockPaper.js
@@ -1,121 +1,81 @@
-const rock=[0,'rock'];
-const paper=[1,'paper'];
-const scissors=[2,'scissors'];
+const rock=[0,'ROCK'];
+const paper=[1,'PAPER'];
+const scissors=[2,'SCISSORS'];
+const selectRock=document.getElementById('btn-rock');
+const selectPaper=document.getElementById('btn-paper');
+const selectScissors=document.getElementById('btn-scissors');
+const displayResult=document.getElementById('display-result');
+const playerSelection=document.getElementById('player-choice')
+const compSelection=document.getElementById('computer-choice')
 
-function randomNumGenerator(){
+
+selectRock.addEventListener('click',()=>{
+    let player=selectRock.textContent;
+    playerSelection.textContent=`Player choice is ${player}`
+    let computer=computerChoice();
+    compSelection.textContent=`Computer choice is ${computer}`
+    playGame(player,computer)
+})
+
+selectPaper.addEventListener('click',()=>{
+    let player=selectPaper.textContent;
+    playerSelection.textContent=`Player choice is ${player}`
+    let computer=computerChoice();
+    compSelection.textContent=`Computer choice is ${computer}`
+    playGame(player,computer)
+    })
+selectScissors.addEventListener('click',()=>{
+    let player=selectScissors.textContent;
+    playerSelection.textContent=`Player choice is ${player}`
+    let computer=computerChoice();
+    compSelection.textContent=`Computer choice is ${computer}`
+    playGame(player,computer)
+})
+
+const randomNumGenerator=()=>{
     let rand= Math.floor(Math.random()*3);
     return rand;
 };
  
 
 
-function computerChoice(){
+const computerChoice=()=>{
     let random=randomNumGenerator();
    
     if(random===rock[0]){
-        console.log(rock[1])
-        return(rock[0])
+        return(rock[1])
     }else if(random===paper[0]){
-        console.log(paper[1])
-        return(paper[0])
+        return(paper[1])
     }else if(random===scissors[0]){
-        console.log(scissors[1])
-        return(scissors[0])
+        return(scissors[1])
     }
 }
 
 
-function playerChoice(){
-    let playerInput=prompt("please make a choice:");
-    playerInput=playerInput.toLowerCase();
-    if(playerInput===rock[1]){
-        console.log(rock[1])
-        return(rock[0])
-    } 
-    else if(playerInput===paper[1]){
-        console.log(paper[1])
-        return(paper[0])
-    } 
-    else if(playerInput===scissors[1]){
-        console.log(scissors[1])
-        return(scissors[0])
-    } 
-    else{
-        console.log("please make a correct choice!")
-    }
-    
-}
 
-
-
-
-function game(player,computer){
-    
-   
-    
-    breakme:if(player==computer){
-        console.log("It is a draw.")
-        return "draw";
-    }
-    else if(player==0 && computer==1){
-        console.log("Computer wins")
-        return "comp wins";
-    }else if(player==1 && computer==2){
-         console.log("Computer wins")
-        return "comp wins";
-    }else if(player==2 && computer==0){
-        console.log("Computer wins")
-        return "comp wins";
-    
-    
-    }else if (player==0 && computer==2){
-        console.log("player wins")
-        return "player wins"
-    }else if (player==1 && computer==0){
-        console.log("player wins")
-        return "player wins"
-    }else if (player==2 && computer==1){
-        console.log("player wins")
-        return "player wins"
-    }
-    else{
-        console.log("please enter a valid choice");
-        break breakme;
-        
-    }
-}
-
-function fiveGame(){
-    let playeScore=0;
-    let computerScore=0;
-    let draw=0;
-    for(i=0; i<5;i++){
-        const p=playerChoice();
-        const c=computerChoice();
-        let result=game(p,c);
-        if(result=="player wins"){
-            playeScore+=1;
-            console.log("your score is "+playeScore);
-
-        }else if(result=="comp wins"){
-            computerScore+=1;
-            console.log("Computer score is "+computerScore);
-        }else{
-            draw+=1;
-            console.log("Draw score is "+draw);
-
+const playGame=(player,computer)=>{
+    if(player==computer){
+        displayResult.textContent="Its a draw"
+     }else if(player==rock[1] && computer==paper[1]){
+         displayResult.textContent=`Computer Wins`
+         return 'comp wins'
+     }else if(player==paper[1] && computer==scissors[1]){
+         displayResult.textContent=`Computer Wins`
+         return 'comp wins'
+     }else if(player==scissors[1] && computer==rock[1]){
+         displayResult.textContent=`Computer Wins`
+         return 'comp wins'
+     }else if (player==rock[1] && computer==scissors[1]){
+         displayResult.textContent=`Player Wins`
+         return 'player wins'
+     }else if (player==paper[1] && computer==rock[1]){
+         displayResult.textContent=`Player Wins`
+         return 'player wins'
+     }else if (player==scissors[1] && computer==paper[1]){
+         displayResult.textContent=`Player Wins`
+         return 'player wins'
+     }
+     else{
+         console.log("please enter a valid choice");
         }
-
     }
-
-    if(playeScore>computerScore){
-        console.log("player wins the game ")
-    }else if(playeScore<computerScore){
-        console.log("computer wins the game")
-    }else{
-        console.log("it is a draw")
-    }
-
-}
-
-fiveGame();
